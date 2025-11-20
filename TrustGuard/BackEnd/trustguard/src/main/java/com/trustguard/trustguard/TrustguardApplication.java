b9d816f9ba0c51463b0ee393146418cd4386895d
GabiRodrigues04 <gabriela.arodrigues004@gmail.com>
2025-11-20 12:44:51 -0300

    feat(backend): inicia backend com java spring

---
+++ TrustGuard/BackEnd/trustguard/src/main/java/com/trustguard/trustguard/TrustguardApplication.java
@@ -0,0 +1,13 @@
+package com.trustguard.trustguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustguardApplication.class, args);
+	}
+
+}
